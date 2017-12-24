30282edfce3d9d1e4a1f946f790afff23e992266
Sakai Toma <o_edo@hotmail.com>
2017-12-24 23:01:42 +0100

    First correct attempt with web worker.

---
+++ exercises/static/js/codeEval.js
@@ -0,0 +1,42 @@
+var window = {};
+window.alert = function(){
+    console.log.apply(console, ["Alert: "].concat(Array.prototype.slice.call(arguments)));
+};
+var alert = window.alert;
+var console = {
+    log: function(){
+        var str = "";
+        for(var i = 0; i < arguments.length; i++){
+            str += JSON.stringify(arguments[i]) + " ";
+        }
+        str += "\n";
+        // send the message back to the main thread
+        self.postMessage(str);
+    },
+    error: function(){
+        console.log.apply(console, ["ERROR: "].concat(Array.prototype.slice.call(arguments)));
+    },
+    warn: function(){
+        console.log.apply(console, ["WARNING: "].concat(Array.prototype.slice.call(arguments)));
+    }
+};
+
+const code = `const insertInOrder = function( element, arr ){
+    if( arr.length === 0){
+        return [element];
+    }
+    else if( element <= arr[0] ){
+        return [element].concat(arr);
+    }
+    else{
+        return arr.slice(1).concat( insertInOrder() );
+    }
+};
+console.log( insertInOrder(80, [8,10,111,1111] ) );
+`;
+try{
+    eval ( code );
+}
+catch(e){
+    self.postMessage(e.stack);
+}
