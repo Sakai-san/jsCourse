c9887b73753efcb37ba0c7d65f36c1cced064178
Sakai Toma <o_edo@hotmail.com>
2018-01-18 23:31:27 +0100

    Add query string parsing.

--- exercises/static/js/studybyyourself_autocorrection.js
+++ exercises/static/js/studybyyourself_autocorrection.js
@@ -5,6 +5,22 @@ editor.setFontSize("16px");
 editor.setShowPrintMargin(false);
 editor.getSession().setTabSize(2);
 
+var getUrlParameter = function getUrlParameter(sParam) {
+    var sPageURL = decodeURIComponent(window.location.search.substring(1)),
+        sURLVariables = sPageURL.split('&'),
+        sParameterName,
+        i;
+
+    for (i = 0; i < sURLVariables.length; i++) {
+        sParameterName = sURLVariables[i].split('=');
+
+        if (sParameterName[0] === sParam) {
+            return sParameterName[1] === undefined ? true : sParameterName[1];
+        }
+    }
+};
+console.log(getUrlParameter('exercise') );
+
 var workerFactoy = function( code,  polyfill ){
   var extendedCode    = polyfill + 'try{' + code +'self.postMessage( {eval : accConsoles, error: false} );} catch(e){ self.postMessage( {eval : e.stack, error: true} );}';
 
